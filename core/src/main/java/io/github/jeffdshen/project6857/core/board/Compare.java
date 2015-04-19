1e2860d2a5d4948e8777e921dcd2836a038e2486
Peinan Chen <peinanchen@gmail.com>
2015-04-18 22:28:04 -0400

    Board done but no tests yet

---
+++ core/src/main/java/io/github/jeffdshen/project6857/core/board/Compare.java
@@ -0,0 +1,8 @@
+package io.github.jeffdshen.project6857.core.board;
+
+/**
+ * Created by chenp on 4/18/2015.
+ */
+public enum Compare {
+    WIN, LOSS, TIE;
+}
---
+++ core/src/main/java/io/github/jeffdshen/project6857/core/board/Move.java
@@ -0,0 +1,7 @@
+package io.github.jeffdshen.project6857.core.board;
+
+/**
+ * Created by chenp on 4/18/2015.
+ */
+public class Move {
+}
