4448ee1f470a18303b8c626b168ec96c40a09b0d
jdshen <jdshen@mit.edu>
2015-04-18 22:45:32 -0400

    Example test

---
+++ core/src/test/java/io/github/jeffdshen/project6857/core/ExampleTest.java
@@ -0,0 +1,12 @@
+package io.github.jeffdshen.project6857.core;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ExampleTest {
+    @Test
+    public void testExampleMethod() throws Exception {
+        assertEquals(1, 1);
+    }
+}
